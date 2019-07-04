9dfeb05721d2e73c47bf142e467281742762fe09
nishide <nishide.h@gmail.com>
2019-07-04 15:33:40 +0900

    Update yarn build

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <div class='link'>
+      <router-link to='/'>Home</router-link>
+      <div class='clear'></div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+    }
+  }
+}
+</script>
