d8637c49a65d9d9455e03cfd2e96e73926f135e2
nishide <nishide.h@gmail.com>
2019-07-04 15:31:05 +0900

    Add HomePage and Del HelloWorld

---
+++ src/pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <transition appear>
+    <div id="hello">
+      <h1>{{ msg }}</h1>
+      <h2>This portfolio is built with Vue.js + Webpack + HTML5 + CSS3.</h2>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  title: ' ',
+  data () {
+    return {
+      msg: 'Welcome to nishide-h Portfolio Site.'
+    }
+  }
+}
+</script>
